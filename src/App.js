0b9969d0d225fd1058b2f0ec59ffa3d6e9139936
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-26 12:26:56 +0200

    Add routing to menu-items

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+    </Switch>
+  );
 }
 
 export default App;
